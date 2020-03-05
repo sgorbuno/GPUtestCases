2a6a03242853ff38319e1998f5d701958f6a997f
Sergey Gorbunov <sergey.gorbunov@fias.uni-frankfurt.de>
2020-03-05 01:09:35 +0000

    update

--- maddLoop/maddLoop.cpp
+++ maddLoop/maddLoop.cpp
@@ -5,8 +5,7 @@
   nvcc maddLoop.cu --ptx
   nvcc maddLoop.cu
 
-  ./a.out
-
+  CUDA_VISIBLE_DEVICES=0 ./a.out
 
 */
 
@@ -41,43 +40,34 @@ const char *gpuName = "NVIDIA";
 #include <clocale>
 #include <cstdio>
 
-constexpr int nUnroll = 64;
+#define nUnroll 1
+
+constexpr int nnUnroll = nUnroll;
 
 __global__ void test(int *gN, float b, float c, float d, int *out) {
 
-  // if (threadIdx.x) return;
   const int n = *gN;
-  b += threadIdx.x;
-#pragma unroll(nUnroll)
-  for (int i = 0; i < n; i++) {
-    b *= c;
-    b += d;
-  }
-  out[threadIdx.x] = b;
-}
+  float x = b + threadIdx.x;
 
-/*
-__global__ void test(int *gN, float b, float c, float d, int *out) {
-  const int n = *gN;
-  b += threadIdx.x;
-  #pragma unroll(1)
+#if nUnroll < 0    // no unrolling
+#elif nUnroll == 0 // automatic
+#pragma unroll
+#elif nUnroll > 0 // manual
+#pragma unroll(nnUnroll)
+#endif
+
   for (int i = 0; i < n; i++) {
-    if (threadIdx.x == 0) {
-      b *= c;
-      b += d;
-    }
+    x *= c;
+    x += d;
   }
-  //if (threadIdx.x == 0)
-    out[threadIdx.x] = b;
+  out[threadIdx.x] = x;
 }
-*/
 
 int main(int argc, char **argv) {
 
-  // const int GlobalArraySIZE = 1 * 1024 * 1024 * 1024 / sizeof(int); // 1 GB
   const int GlobalArraySIZE = 1024; //
 
-  const int nIter = 5;
+  const int nMeasurements = 5;
 
   float b = 10, c = 1, d = 0;
 
@@ -97,15 +87,17 @@ int main(int argc, char **argv) {
              cudaMemcpyHostToDevice);
 
   cudaStream_t stream;
-  cudaEvent_t start[nIter], stop[nIter];
+  cudaEvent_t start[nMeasurements], stop[nMeasurements];
   cudaStreamCreate(&stream);
 
-  for (int i = 0; i < nIter; i++) {
+  for (int i = 0; i < nMeasurements; i++) {
     cudaEventCreate(&start[i]);
     cudaEventCreate(&stop[i]);
   }
 
-  for (int nLoops = 1; nLoops <= 1000000000; nLoops *= 10) {
+  // for (int nLoops = 1; nLoops <= 1000000000; nLoops *= 10) {
+  for (int nLoops = 100 * 1000 * 1000; nLoops <= 100 * 1000 * 1000;
+       nLoops *= 10) {
 
     hostPtr1[0] = nLoops;
     cudaMemcpy(devPtr1, hostPtr1, sizeof(int), cudaMemcpyHostToDevice);
@@ -117,7 +109,7 @@ int main(int argc, char **argv) {
 
       printf("\n");
 
-      for (int iter = 0; iter < nIter; iter++) {
+      for (int iter = 0; iter < nMeasurements; iter++) {
         cudaEventRecord(start[iter], stream);
 #ifdef __HIPCC__
         hipLaunchKernelGGL(test, dim3(nBlocks), dim3(nThreads), 0, stream,
@@ -128,7 +120,7 @@ int main(int argc, char **argv) {
         cudaEventRecord(stop[iter], stream);
       }
 
-      for (int iter = 0; iter < nIter; iter++) {
+      for (int iter = 0; iter < nMeasurements; iter++) {
         cudaEventSynchronize(stop[iter]);
         float timeMS;
         cudaEventElapsedTime(&timeMS, start[iter], stop[iter]);
@@ -140,6 +132,7 @@ int main(int argc, char **argv) {
         }
         setlocale(LC_NUMERIC, "");
         setlocale(LC_ALL, "");
+
         printf("%s  %'d loops. Unroll %d, NThreads %d. LoopTest Time %'d usec, "
                "output: %d\n",
                gpuName, nLoops, nUnroll, nThreads, (int)rint(timeMS * 1000),
